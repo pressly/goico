490b523fe30e5c5eeb4d15b296a61594e7e1a55f
David Kua <david.kua@pressly.com>
2015-02-06 18:27:40 +0000

    Updated the test

--- reader_test.go
+++ reader_test.go
@@ -17,11 +17,12 @@ const (
 )
 
 func TestDecodeAll(t *testing.T) {
+	t.Parallel()
 	assert := assert.New(t)
 	files, _ := filepath.Glob("testdata/favicons/*.ico")
-	for _, f := range files {
+	for i, f := range files {
 		fmt.Println()
-		fmt.Println("WORKING WITH", f)
+		fmt.Println(i, "WORKING WITH", f)
 		fmt.Println()
 		icoData, err := ioutil.ReadFile(f)
 		assert.NoError(err, f)
@@ -51,17 +52,6 @@ func TestDecodeAll(t *testing.T) {
 			}
 
 			assert.Equal(im.Bounds(), jpgImage.Bounds())
-			// TODO: Check for pixel color equality between jpgs generated with imagemagick, and our renderer
-			/*for i := im.Bounds().Min.X; i <= im.Bounds().Max.X; i++ {
-				for j := im.Bounds().Min.Y; j <= im.Bounds().Max.Y; j++ {
-					r, g, b, a := im.At(i, j).RGBA()
-					r2, g2, b2, a2 := jpgImage.At(i, j).RGBA()
-					assert.Equal(r, r2, fmt.Sprintf("%s: red at %d, %d", f, i, j))
-					assert.Equal(g, g2, fmt.Sprintf("%s: green at %d, %d", f, i, j))
-					assert.Equal(b, b2, fmt.Sprintf("%s: blue at %d, %d", f, i, j))
-					assert.Equal(a, a2, fmt.Sprintf("%s: alpha at %d, %d", f, i, j))
-				}
-			} */
 		}
 	}
 }
